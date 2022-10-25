58fc00481cbd771b4927810786974aace7b80ac6
nusratnova5 <nusrat.nova16@gmail.com>
2022-10-25 23:06:48 +0600

    api created for loading each course details

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('/courses',(req,res)=>{
     res.send(coursesInfo);
 });
 
+app.get('/courses/:name',(req,res)=>{
+    const name =req.params.name;
+    const getCourseInfo=coursesInfo?.filter(course=>course.name === name);
+    res.send(getCourseInfo);
+});
+
+
+
 app.listen(port,()=>{
     console.log('Courses server running on port',port);
 });
